07b502ec7872de6219dece2d27bfe1cd864dd10d
Naman Arora <naman30103@gmail.com>
2024-09-12 00:47:31 +0530

    static root

--- Backend/capstone/capstone/settings.py
+++ Backend/capstone/capstone/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL2 = '/media/'
 # settings.py
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
